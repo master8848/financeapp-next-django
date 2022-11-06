d7e3c5023f7196d4ee858bd49d5976a00c6dd690
masterboysks1 <saurav@spellinnovation.com>
2022-11-06 11:47:57 +0545

    darkmode by default may be make it a state variable

--- frontend-stable/pages/_app.tsx
+++ frontend-stable/pages/_app.tsx
@@ -4,7 +4,7 @@ import DevNavbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <body className="bg-gray-100 dark:bg-black min-w-[100vw]  min-h-screen dark:text-white">
+    <body className="bg-gray-100 dark:bg-black min-w-[100vw]  min-h-screen dark:text-white dark">
       <DevNavbar />
       <Component {...pageProps} />
     </body>
